46bcf2d203514983467211edcad9991843b654b6
2530228-spec <2530228@upv.edu.mx>
2025-11-13 10:34:34 -0600

    true false

---
+++ src/understanding_if_statement.py
@@ -0,0 +1,7 @@
+cars = ["audi", "bmw", "chevrolet", "corvette", "tesla"]
+
+for car in cars:
+    if car == "bmw" or car == "tesla" or car == "audi": 
+        print(car.upper())
+    else:
+        print(car)   
